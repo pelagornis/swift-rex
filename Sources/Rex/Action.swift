1ba1bc0966f4d06efc5f4b5b965c175d5f57e694
jihoonahn <jihoonahn.dev@gmail.com>
2025-08-16 00:32:13 +0900

    Update Action

--- Sources/Rex/Action.swift
+++ Sources/Rex/Action.swift
@@ -1,3 +1,19 @@
 import Foundation
 
+/// A protocol that defines the base type for all actions in the application.
+///
+/// Actions represent events that can occur in your application and are used
+/// to describe state changes. All actions should conform to this protocol
+/// to ensure type safety and consistency.
+///
+/// ## Example
+/// ```swift
+/// enum AppAction: ActionType {
+///     case increment
+///     case decrement
+///     case setCount(Int)
+///     case loadUser
+///     case userLoaded(User)
+/// }
+/// ```
 public protocol ActionType: Sendable {}
